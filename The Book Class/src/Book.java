bf197e9223ca6bbc3377d3bb36dbfef3bb14455f
BOY14212528 <BOY14212528@BR-COMP-TECH.serc.ac.uk>
2018-01-23 11:06:37 +0000

    Change-Id: I9fcd5b6536636496a37489f9633da0e78ee48e8d

---
+++ The Book Class/src/Book.java
@@ -0,0 +1,12 @@
+
+public class Book {
+	
+	public Object title;
+	String author;
+
+	public Book(String string) {
+	this.title=string;
+	this.author ="unknown author";
+	}
+
+}
--- The Book Class/src/BookTest.java
+++ The Book Class/src/BookTest.java
@@ -5,9 +5,13 @@ import org.junit.Test;
 public class BookTest {
 
 	@Test
-	public void test() {
-	Book b1 = new Book("Great "
-			+ "Expectations";
-	}
-
+	public void testBook() {
+	Book b1 = new Book("Great Expectations");
+	
+	assertEquals("Great Expectations", 
+		b1.title);
+	
+	assertEquals("unknown author", 
+			b1.author);
+		}
 }
